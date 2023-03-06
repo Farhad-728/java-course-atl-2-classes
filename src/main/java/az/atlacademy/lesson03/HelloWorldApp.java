f2108ddd678d4686b16fcc536dbf4d4ded3e7f17
Farhad-728 <farhaad1987@gmail.com>
2023-03-06 19:41:50 +0400

    Lessons5 commit OperatorsApp class

--- src/main/java/az/atlacademy/lesson03/HelloWorldApp.java
+++ src/main/java/az/atlacademy/lesson03/HelloWorldApp.java
@@ -1,7 +1,10 @@
 package az.atlacademy.lesson03;
 
-public class HelloWorldApp {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+//public class HelloWorldApp {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//        System.out.print("Hello, World!");
+//        System.out.printf("Hello, %s", "World");
+//        System.out.printf("Hello, %s\n", "World");
+//    }
+//}
---
+++ src/main/java/az/atlacademy/lesson05/OperatorsApp.java
@@ -0,0 +1,15 @@
+package az.atlacademy.lesson05;
+
+public class OperatorsApp {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        double c = 10.0;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(c / a);
+
+    }
+}
