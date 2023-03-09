2ec70bb8a9725f52a14bc3fd882c3c3cffb36688
Farhad-728 <farhaad1987@gmail.com>
2023-03-09 19:11:54 +0400

    Lessons7 NumberApp class

--- src/main/java/az/atlacademy/lesson6/ScannerApp.java
+++ src/main/java/az/atlacademy/lesson6/ScannerApp.java
@@ -1,6 +1,5 @@
 package az.atlacademy.lesson6;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -13,7 +12,7 @@ public class ScannerApp {
         System.out.printf("%.5f", num);
     }
 
-    public static void main(String[] args) {
+    public static void mainV3(String[] args) {
         //Input
         Scanner scan = new Scanner(System.in);
         String word = scan.nextLine();
@@ -25,4 +24,56 @@ public class ScannerApp {
         System.out.println(word.trim());
         System.out.println(word);
     }
+
+    public static void main(String[] args) {
+        //Input
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Yanvar");
+                break;
+            case 2:
+                System.out.println("Fevral");
+                break;
+            case 3:
+                System.out.println("Mart");
+                break;
+            case 4:
+                System.out.println("Aprel");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("Iyun");
+                break;
+            case 7:
+                System.out.println("Iyul");
+                break;
+            case 8:
+                System.out.println("Avqust");
+                break;
+            case 9:
+                System.out.println("Sentyabr");
+                break;
+            case 10:
+                System.out.println("Oktyabr");
+                break;
+            case 11:
+                System.out.println("Noyabr");
+                break;
+            case 12:
+                System.out.println("Dekabr");
+                break;
+            default:
+                System.out.println("Duzgun daxil edilmeyib");
+                break;
+        }
+
+    }
+
 }
+
+
