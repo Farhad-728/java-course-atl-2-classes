e24969682b01506f51282215193a0dc2dd2e14ef
Farhad-728 <farhaad1987@gmail.com>
2023-03-25 20:41:44 +0400

    Lesson10 task Water and Ice app

---
+++ src/main/java/az/atlacademy/lesson10/WaterAndIceApp.java
@@ -0,0 +1,15 @@
+package az.atlacademy.lesson10;
+
+import java.util.Scanner;
+
+public class WaterAndIceApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        if (t > 0) {
+            System.out.println("Water");
+        } else {
+            System.out.println("Ice");
+        }
+    }
+}
