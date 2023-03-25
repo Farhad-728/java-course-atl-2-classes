e2935c884ca6f21d04e69388f2b6e777bb992931
Farhad-728 <farhaad1987@gmail.com>
2023-03-25 18:34:06 +0400

    Lesson10 task Conditional Statement app

---
+++ src/main/java/az/atlacademy/lesson10/CondStatementApp.java
@@ -0,0 +1,17 @@
+package az.atlacademy.lesson10;
+
+import java.util.Scanner;
+
+public class CondStatementApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        if (x < 5) {
+            int y = (x * x) - (3 * x) + 4;
+            System.out.println(y);
+        } else if (x >= 5) {
+            int y = x + 7;
+            System.out.println(y);
+        }
+    }
+}
