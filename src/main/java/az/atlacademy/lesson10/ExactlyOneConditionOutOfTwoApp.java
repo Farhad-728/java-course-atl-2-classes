8d5760bb5c87fcdb87182bd96528872186770587
Farhad-728 <farhaad1987@gmail.com>
2023-03-26 01:41:46 +0400

    Lesson10 task exactly one condition out of two app

---
+++ src/main/java/az/atlacademy/lesson10/ExactlyOneConditionOutOfTwoApp.java
@@ -0,0 +1,15 @@
+package az.atlacademy.lesson10;
+
+import java.util.Scanner;
+
+public class ExactlyOneConditionOutOfTwoApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if (n % 2 == 0 | (n < 0 && n % 3 == 0)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
