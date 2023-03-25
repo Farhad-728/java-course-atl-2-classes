b101913df62975219df461e1438c366b3f2d548e
Farhad-728 <farhaad1987@gmail.com>
2023-03-25 18:25:41 +0400

    Lesson10 task EvenOdd app

---
+++ src/main/java/az/atlacademy/lesson10/EvenOddApp.java
@@ -0,0 +1,15 @@
+package az.atlacademy.lesson10;
+
+import java.util.Scanner;
+
+public class EvenOddApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("EVEN");
+        } else {
+            System.out.println("ODD");
+        }
+    }
+}
