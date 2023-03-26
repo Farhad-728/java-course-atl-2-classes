ec54a2d1940715ef1aff5aa3daa7c1e1d7548ebe
Farhad-728 <farhaad1987@gmail.com>
2023-03-26 12:26:40 +0400

    Lesson10 task none of the two conditions app

---
+++ src/main/java/az/atlacademy/lesson10/NoneOfTheTwoConditionsApp.java
@@ -0,0 +1,17 @@
+package az.atlacademy.lesson10;
+
+import java.util.Scanner;
+
+public class NoneOfTheTwoConditionsApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if (n % 2 == 0 && n > 0) {
+            System.out.println("NO");
+        } else if (n % 2 != 0 && n < 0) {
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
+        }
+    }
+}
