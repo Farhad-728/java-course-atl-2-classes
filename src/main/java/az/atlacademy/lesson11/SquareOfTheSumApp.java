aee43f102294d4e2f8053b6d1f113c49792e2e23
Farhad-728 <farhaad1987@gmail.com>
2023-03-26 15:25:58 +0400

    Lesson11 task square of the sum app

---
+++ src/main/java/az/atlacademy/lesson11/SquareOfTheSumApp.java
@@ -0,0 +1,14 @@
+package az.atlacademy.lesson11;
+
+import java.util.Scanner;
+
+public class SquareOfTheSumApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int firstdDigit = num % 10;
+        int secondDigit = (num / 10) % 10;
+        int sum = firstdDigit + secondDigit;
+        System.out.println(sum * sum);
+    }
+}
