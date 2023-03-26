93b086c5415686b4ea189df8f5aae35e52afe7d6
Farhad-728 <farhaad1987@gmail.com>
2023-03-26 14:14:53 +0400

    Lesson10 task median number app

---
+++ src/main/java/az/atlacademy/lesson11/MedianNumberApp.java
@@ -0,0 +1,17 @@
+package az.atlacademy.lesson11;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MedianNumberApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] number = new int[3];
+        for (int i = 0; i < number.length; i++) {
+            number[i] = scan.nextInt();
+        }
+        Arrays.sort(number);
+        System.out.println(number[1]);
+    }
+}
+
