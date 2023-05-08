146a1280958bc862f3c6b5ea9b7ab0e9d6280420
Farhad-728 <farhaad1987@gmail.com>
2023-05-09 00:06:59 +0400

    Lesson14pr1 classwork task PersonApp

--- src/main/java/az/atlacademy/lesson14pr1/Person.java
+++ src/main/java/az/atlacademy/lesson14pr1/Person.java
@@ -1,24 +1,24 @@
 package az.atlacademy.lesson14pr1;
 
 public class Person implements Comparable<Person> {
-    private int Id;
+    private int id;
     private int age;
     private double salary;
     private String name;
 
     public Person(int id, int age, double salary, String name) {
-        Id = id;
+        this.id = id;
         this.age = age;
         this.salary = salary;
         this.name = name;
     }
 
     public int getId() {
-        return Id;
+        return id;
     }
 
     public void setId(int id) {
-        Id = id;
+        id = id;
     }
 
     public int getAge() {
@@ -47,11 +47,11 @@ public class Person implements Comparable<Person> {
 
     @Override
     public String toString() {
-        return String.format("Person{Id=%d, age=%d, salary=%s, name='%s'}", Id, age, salary, name);
+        return String.format("Person{Id=%d, age=%d, salary=%s, name='%s'}", id, age, salary, name);
     }
 
     @Override
-    public int compareTo(Person person) {
-        return Integer.compare(this.Id, person.Id);
+    public int compareTo(Person that) {
+        return Integer.compare(this.id, that.id);
     }
 }
