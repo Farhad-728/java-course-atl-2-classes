da0fca3a28c82cc8d7271b0a2d20b438cee526fe
Farhad-728 <farhaad1987@gmail.com>
2023-05-08 23:41:47 +0400

    Lesson14pr1 classwork task LinearSearchApp

---
+++ src/main/java/az/atlacademy/lesson14pr1/LinearSearchApp.java
@@ -0,0 +1,26 @@
+package az.atlacademy.lesson14pr1;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class LinearSearchApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] nums = new int[15];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * 101);
+            System.out.print(nums[i] + " ");
+        }
+        int n = scan.nextInt();
+        System.out.println(findNumArray(nums, n));
+    }
+
+    public static Optional<Integer> findNumArray(int[] nums, int num) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                return Optional.of(i);
+            }
+        }
+        return Optional.empty();
+    }
+}
