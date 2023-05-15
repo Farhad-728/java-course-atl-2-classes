42821ea94f0d75a951e0f3942f53c5f6e73d9da8
Farhad-728 <farhaad1987@gmail.com>
2023-05-15 19:49:09 +0400

    Classroom LinkedListAppListApp

---
+++ src/main/java/az/atlacademy/lesson16pr1/LinkedListApp.java
@@ -0,0 +1,24 @@
+package az.atlacademy.lesson16pr1;
+
+import java.util.LinkedList;
+
+public class LinkedListApp {
+    public static void main(String[] args) {
+        LinkedList<String> name = new LinkedList<>();
+        name.add("Elvin");
+        name.add("Samir");
+        name.add("Ilqar");
+        name.add("Nizami");
+        System.out.println(name);
+
+        name.addFirst("Surxay");
+        System.out.println(name);
+
+        name.addLast("Orkhan");
+        System.out.println(name);
+
+        System.out.println(name.getFirst());
+
+        System.out.println(name.getLast());
+    }
+}
