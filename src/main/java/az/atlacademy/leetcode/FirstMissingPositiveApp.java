7e8340b0b904bf8881cae15b4e863a66af8652c7
Farhad-728 <farhaad1987@gmail.com>
2023-05-30 23:31:05 +0400

    Leetcode code attached

--- src/main/java/az/atlacademy/leetcode/FirstMissingPositiveApp.java
+++ src/main/java/az/atlacademy/leetcode/FirstMissingPositiveApp.java
@@ -1,25 +1,25 @@
 package az.atlacademy.leetcode;
 
 import java.util.Arrays;
-
 public class FirstMissingPositiveApp {
 
     public static void main(String[] args) {
-        int[] nums = {2, 4, -1, 1};
-        int n = nums.length;
-        int posNum = firsMissPosNum(nums, n);
+        int[] nums = {-4, 2, 5, 1, 3, 4, 6};
+        int posNum = firsMissPosNum(nums);
         System.out.println(posNum);
     }
 
-    private static int firsMissPosNum(int[] arr, int n) {
-        Arrays.sort(arr);
+    private static int firsMissPosNum(int[] arr) {
+        Arrays.sort(arr);  // -4, 0, 1, 2, 3, 5, 6, 7, 8, 11, 14
         int minPosNum = 1;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == minPosNum) {
+        for (int j : arr) {
+            if (j == minPosNum) {
                 minPosNum++;
+            } else if (j > 0) {
+                return minPosNum;
             }
         }
-        return minPosNum;
+        return (++arr[arr.length - 1]);
     }
 }
 
