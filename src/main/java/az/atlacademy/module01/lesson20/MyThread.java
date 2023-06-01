15b3493cf50f6acdc5c9fae6157141f78b5d2f77
Farhad-728 <farhaad1987@gmail.com>
2023-06-01 19:27:26 +0400

    ClassRoom code MyThread attached

--- src/main/java/az/atlacademy/module01/lesson20/MyThread.java
+++ src/main/java/az/atlacademy/module01/lesson20/MyThread.java
@@ -3,9 +3,15 @@ package az.atlacademy.module01.lesson20;
 public class MyThread extends Thread {
 
     @Override
+    public synchronized void start() {
+        super.start();
+    }
+
+    @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println(new MyThread().getClass().getName());
         }
+
     }
 }
