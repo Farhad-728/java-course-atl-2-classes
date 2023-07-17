e4202c750dfff4f3ba3a6f5a53daa19fef7f43e5
Farhad-728 <farhaad1987@gmail.com>
2023-07-17 20:04:48 +0400

    ClassRoom jdbc commit code

---
+++ src/main/java/az/atlacademy/module02/JdbcApp.java
@@ -0,0 +1,40 @@
+package az.atlacademy.module02;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class JdbcApp {
+
+    public static final String getAllStudentsSql = "SELECT * FROM students;";
+
+    public static void main(String[] args) {
+        getAllStudents().forEach(System.out::println);
+    }
+
+    private static List<Student> getAllStudents() {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/postgres",
+                "postgres",
+                "postgres")) {
+            PreparedStatement query = conn.prepareStatement(getAllStudentsSql);
+            ResultSet resultSet = query.executeQuery();
+            List<Student> students = new ArrayList<>();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String pin = resultSet.getString("pin");
+                students.add(new Student(id, name, pin));
+            }
+            return students;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
+}
